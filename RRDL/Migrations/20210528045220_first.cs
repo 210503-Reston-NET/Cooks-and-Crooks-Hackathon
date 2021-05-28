5c3d33cc4e6d682114b0f16603eaf94244603c0a
sehar-malik <sehar.malik508@gmail.com>
2021-05-28 08:28:36 -0700

    changes to apply after stash

---
+++ RRDL/Migrations/20210528045220_first.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RRDL.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "fileName",
+                table: "Restaurants",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "fileName",
+                table: "Restaurants");
+        }
+    }
+}
